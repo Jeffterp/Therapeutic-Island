3e9ac0c397e69ce1b62da80334fe395f0104b381
brian <brianquach123@gmail.com>
2017-03-01 19:02:29 -0500

    first upload to gitrepository for semester project

---
+++ Semester Project/Assets/Car_script.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car_script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Drive ();
+	}
+
+	void Drive(){
+		gameObject.transform.Translate (0, 0, -.01f);
+	}
+}
---
+++ Semester Project/Assets/player_script.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_script : MonoBehaviour {
+	public Terrain terrain;
+	// Use this for initialization
+	void Start () {
+		Terrain terrain_clone = Instantiate (terrain); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
